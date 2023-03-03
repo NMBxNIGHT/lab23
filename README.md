088412e901795d15223391521ef5c7af4cbfc785
NMBxNIGHT <thiranat_kakanmee@cmu.ac.th>
2023-03-03 15:15:41 +0700

    sed al

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NAME:
-# STUDENT ID:
+# NAME:thiranat kakanmee
+# STUDENT ID:650610762
--- average.cpp
+++ average.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc,char *argv[]){
+    if(argc == 1){
+        cout << "Please input numbers to find average.";
+    }else {
+        double aver=0;
+        for(int i=1;i<argc;i++){
+            aver+= atof(argv[i]);
+        }
+        aver = aver/(argc-1);
+        cout << "---------------------------------\n";
+        cout << "Average of " << argc-1 << " numbers = " << aver; 
+        cout << "\n---------------------------------\n";
+
+    }
+    return 0;
+}
